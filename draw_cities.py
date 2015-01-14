20ab5179329304862a1629b7d0e3e69f6409e6db
salceson <ciolczyk.michal.93@gmail.com>
2015-01-14 11:55:48 +0100

    Drawing cities

--- draw_cities.py
+++ draw_cities.py
@@ -33,7 +33,7 @@ def draw_path(cities, canvas, width, height):
 
     n = len(cities)
     (x1, y1) = cities[0]
-    for i in xrange(1, n+1):
+    for i in xrange(1, n + 1):
         (x2, y2) = cities[i % n]
         canvas.plot([x1, x2], [y1, y2], color='r', linestyle='-', linewidth=0.4)
         (x1, y1) = (x2, y2)
@@ -43,18 +43,20 @@ def draw_path(cities, canvas, width, height):
 
 
 def get_cities_from_file(filename):
-        cities = []
-        with open(filename, "r") as input:
-            input.readline()  # skip first line
-            for line in input:
-                name, x, y = line.strip().split(",")
-                cities.append(City(name, int(x), int(y)))
-        return cities
+    cities = []
+    with open(filename, "r") as input:
+        input.readline()  # skip first line
+        for line in input:
+            name, x, y = line.strip().split(",")
+            cities.append(City(name, int(x), int(y)))
+    return cities
+
 
 if __name__ == "__main__":
     width = 1200
     height = 1200
-    genotype = [28, 3, 17, 0, 11, 29, 9, 22, 19, 6, 7, 1, 12, 15, 27, 14, 8, 25, 21, 5, 16, 23, 4, 24, 26, 2, 20, 10, 18, 13]
+    genotype = [28, 3, 17, 0, 11, 29, 9, 22, 19, 6, 7, 1, 12, 15, 27, 14, 8, 25, 21, 5, 16, 23, 4, 24, 26, 2, 20, 10,
+                18, 13]
     filename = "cities.csv"
 
     cities_list = get_cities_from_file(filename)
